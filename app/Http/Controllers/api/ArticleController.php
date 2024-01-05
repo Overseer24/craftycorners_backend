3e2d6c526289066610f05bf15fef1e1d1ebb97bc
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-01-05 11:13:03 +0800

    Update ArticleController.php

--- app/Http/Controllers/api/ArticleController.php
+++ app/Http/Controllers/api/ArticleController.php
@@ -50,7 +50,6 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        dd($article);
         return new ArticleResource($article);
     }
 
