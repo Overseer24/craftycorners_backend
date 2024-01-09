8955606d0785facc97bff2d8ee986d7aa6ca572c
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-01-09 21:45:29 +0800

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,13 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            @if (trim($slot) === 'Laravel')
+            <img src="https://cdn.discordapp.com/attachments/718323595855527976/1194170383583170620/Crafty_Corners_logo_minimalist_and_simple_digital_art.png?ex=65af60d6&is=659cebd6&hm=aa45513591eb38ff5a84135572d3fe2a8a8e4a390fda3be76b17a80481b84aa4&"
+                class="logo" alt="Laravel Logo">
+            @else
+            {{ $slot }}
+            @endif
+        </a>
+    </td>
 </tr>
