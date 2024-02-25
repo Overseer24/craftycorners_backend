fa517405f3d5da9ac2147ed529393e902229d2bf
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-02-25 13:39:35 +0800

    add

--- app/Http/Resources/Post/HomePagePostResource.php
+++ app/Http/Resources/Post/HomePagePostResource.php
@@ -19,17 +19,19 @@ class HomePagePostResource extends JsonResource
               'title'=>$this->title,
               'content'=>$this->content,
               'community_id'=>$this->community_id,
-              'user_id'=>$this->user_id,
               'created_at'=>$this->created_at->format('Y-m-d H:i:s'),
               'updated_at'=>$this->updated_at->format('Y-m-d H:i:s'),
-           'user' => new UserDataResource($this->user),
-              'community'=>$this->community,
            'likes_count'=>$this->likes->count(),
            'comments_count'=>$this->comments->count(),
            'liked_by_user'=>$this->isLikedByUser(auth()->id()),
            'post_type' =>$this->post_type,
            'shares'=>$this->shares,
-
+           'image'=>$this->image,
+           'video'=>$this->video,
+           'link'=>$this->link,
+           'user' => new UserDataResource($this->user),
+              'community'=>$this->community,
+           
        ];
     }
 
