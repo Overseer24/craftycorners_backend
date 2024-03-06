21e3f4e43a1c5b24a16139dd620954ebc20f60f8
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-06 16:36:16 +0800

    Change Channel to Private Message

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -22,7 +22,9 @@ class MessageController extends Controller
             'message' => $request->message
         ]);
     $message->load('sender','receiver');
-        broadcast(new PublicChat(new MessageResource($message)))->toOthers();
+
+    broadcast(new MessageSent(new MessageResource($message)))->toOthers();
+//        broadcast(new PublicChat(new MessageResource($message)))->toOthers();
 
         return new MessageResource($message);
     }
--- routes/channels.php
+++ routes/channels.php
@@ -18,6 +18,6 @@ use Illuminate\Support\Facades\Broadcast;
 //});
 
 //
-//Broadcast::channel('chat-{receiver_id}', function ($user, $receiver_id) {
-//    return (int) $user->id === (int) $receiver_id;
-//});
+Broadcast::channel('chat-{receiver_id}', function ($user, $receiver_id) {
+    return (int) $user->id === (int) $receiver_id;
+});
