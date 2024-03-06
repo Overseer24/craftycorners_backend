92df5a786e38e8c654f3c4e0227990ecc81ca0ba
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-06 20:01:01 +0800

    changes

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -20,9 +20,7 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
-        MessageSent::class => [
-            'App\Listeners\SendMessageNotification',
-        ],
+
     ];
 
     /**
