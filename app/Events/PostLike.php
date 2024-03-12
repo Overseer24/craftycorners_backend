68acdedd55c0f2db7aaf0435f56fa0829125ffbc
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-12 09:31:00 +0800

    adding notification

--- app/Events/PostLike.php
+++ app/Events/PostLike.php
@@ -6,31 +6,21 @@ use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcastNow
+class PostLike
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public $user, $message, $conversation;
-
-
 
     /**
      * Create a new event instance.
      */
-    public function __construct($user,$message, $conversation)
+    public function __construct()
     {
-        $this->user = $user;
-        $this->message = $message;
-        $this->conversation = $conversation;
-
+        //
     }
-
 
     /**
      * Get the channels the event should broadcast on.
@@ -39,22 +29,8 @@ class MessageSent implements ShouldBroadcastNow
      */
     public function broadcastOn(): array
     {
-
-
         return [
-            new PrivateChannel('conversation-' . $this->conversation->id),
-            new PrivateChannel('user-' . $this->user)
+            new PrivateChannel('channel-name'),
         ];
     }
-
-
-    public function broadcastWith()
-    {
-        return [
-            'user' => $this->user,
-            'message' => $this->message
-
-        ];
-    }
-
 }
