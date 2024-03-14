59bc1b2dd65304eb33fedf6720b2b0f1ea54f5ea
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-14 17:01:48 +0800

    Update MessageSent.php

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
     public $user;
