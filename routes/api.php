b45e46b0d24b650eec10f789906e35801d1cb26c
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-14 15:40:59 +0800

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -73,16 +73,8 @@ Route::middleware(['auth:sanctum','negativeWordFilter','verified'])
     ->group(function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::get('/user', function (Request $request) {
-            $user = $request->user();
-            // Fetch the count of unread messages for the user.
-            $unreadMessagesCount = cache()->rememberForever('unreadMessagesCount-' . $user->id, function () use ($user) {
-                return $user->unreadMessages()->count();
-            });
-            // Append the unreadMessagesCount to the user object or create a new response structure.
-            return response()->json([
-                'user' => $user,
-                'unreadMessagesCount' => $unreadMessagesCount,
-            ]);
+            return $request
+                ->user();
         });
 
 
