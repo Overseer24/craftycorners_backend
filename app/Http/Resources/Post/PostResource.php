2fc65e6837630725fba67acfab625a944bd8daa4
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-15 23:59:09 +0800

    Create PostResource.php

---
+++ app/Http/Resources/Post/PostResource.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Resources\Post;
+
+use App\Http\Resources\Comment\CommentResource;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'content' => $this->content,
+            'image' => $this->image,
+            'video' => $this->video,
+            'link' => $this->link,
+            'post_type' => $this->post_type,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->diffForHumans(),
+            'user' =>[
+                 'id' => $this->user->id,
+                'first_name' => $this->user->first_name,
+                'middle_name' =>$this->user->middle_name,
+                'last_name' => $this->user->last_name,
+                'user_name' => $this->user->user_name,
+                'profile_picture' => $this->user->profile_picture,
+                'type' => $this->user->type,
+         ],
+            'community' => new CommunityPostResource($this->community),
+            'likes_count' => $this->likes->count(),
+            'comments' => CommentResource::collection($this->comments),
+            'comments_count' => $this->comments->count(),
+            'shares' => $this->shares,
+        ];
+    }
+
+
+}
