98cbed7f684bf60f757f460c6ab74e3c67590fb2
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-19 17:42:33 +0800

    Create AuthApprovedMentor.php

---
+++ app/Http/Resources/Mentor/AuthApprovedMentor.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources\Mentor;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AuthApprovedMentor extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray($request): array
+    {
+        return [
+            'id' => $this->id,
+            'student_id' => $this->student_id,
+            'program' => $this->Program,
+            'specialization' => $this->specialization,
+            'community' => [
+                'id' => $this->community->id,
+                'name' => $this->community->name,
+            ],
+
+        ];
+    }
+}
