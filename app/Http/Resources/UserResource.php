25b40512f2eea9201ed6ae894c4f981c944cda6b
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-26 22:11:53 +0800

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,25 +20,25 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'profile_picture' => $this->profile_picture,
             'type' => $this->type,
-            'created_at' => $this->created_at->format('Y-m-d H:i:s')
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
 
 //            'assessment_completed'=>$this->pre_assessment_completed,
-//            'communities'=>$this->communities->map(function($community){
-//                $experience  = $this->experiences->firstWhere('community_id', $community->id);
-//                $nextLevelExperience = $this->nextLevelExperience($community->id);
-//                return[
-//                    'id' => $community->id,
-//                    'name' => $community->name,
-//                    'community_photo' => $community->community_photo,
-//                    'description' => $community->description,
-//                    'created_at' => $community->created_at->format('Y-m-d H:i:s'),
-//                    'level' => $experience ? $experience->level : null,
-//                    'experience' => $experience ? $experience->experience_points : null,
-//                    'badge' => $experience ? $experience->badge : null,
-//                    'next_level_experience' => $nextLevelExperience - $experience->experience_points,
-//                ];
+            'communities'=>$this->communities->map(function($community){
+                $experience  = $this->experiences->firstWhere('community_id', $community->id);
+                $nextLevelExperience = $this->nextLevelExperience($community->id);
+                return[
+                    'id' => $community->id,
+                    'name' => $community->name,
+                    'community_photo' => $community->community_photo,
+                    'description' => $community->description,
+                    'created_at' => $community->created_at->format('Y-m-d H:i:s'),
+                    'level' => $experience ? $experience->level : null,
+                    'experience' => $experience ? $experience->experience_points : null,
+                    'badge' => $experience ? $experience->badge : null,
+                    'next_level_experience' => $nextLevelExperience - $experience->experience_points,
+                ];
 
-//            }),
+            }),
 //            'posts'=>UsersPostResource::collection($this->posts),
         ];
     }
