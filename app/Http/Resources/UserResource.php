db29feeaeed0a780dd9e5bd32bc3ec80952cdc39
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-26 22:09:47 +0800

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -10,6 +10,7 @@ class UserResource extends JsonResource
 {
     public function toArray($request)
     {
+
         return [
             'id' => $this->id,
             'first_name' => $this->first_name,
@@ -17,27 +18,9 @@ class UserResource extends JsonResource
             'last_name' => $this->last_name,
             'user_name' => $this->user_name,
             'email' => $this->email,
-            'birthday' => $this->birthday->format('Y-m-d'),
             'profile_picture' => $this->profile_picture,
             'type' => $this->type,
-            'phone_number'=>$this->phone_number,
-            'gender'=>$this->gender,
-            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $this->updated_at->diffForHumans(),
-//            'assessment_completed'=>$this->pre_assessment_completed,
-            'communities'=>$this->communities->map(function($community){
-                return[
-                    'id' => $community->id,
-                    'name' => $community->name,
-                    'community_photo' => $community->community_photo,
-                    'description' => $community->description,
-                    'created_at' => $community->created_at->format('Y-m-d H:i:s'),
-
-                ];
-
-            }),
-     //   'unread_messages' => $this->unreadMessages(),
-//            'posts'=>UsersPostResource::collection($this->posts),
+            'created_at' => $this->created_at->format('Y-m-d H:i:s')
         ];
     }
 }
