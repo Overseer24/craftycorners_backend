92dc6d1eee4d4835776d5e631c8220b182430fd9
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-26 19:57:58 +0800

    Update UserController.php

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -20,7 +20,7 @@ class UserController extends Controller
     public function index()
     {
 
-        $users = User::with('communities')->get();
+        $users = User::with('communities','experiences')->get();
         return UserResource::collection($users);
     }
 
