a3a145cb438b0593b14290e5dfc3d6c9979c762a
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-28 21:15:06 +0800

    test

--- app/Notifications/VerifyEmail.php
+++ app/Notifications/VerifyEmail.php
@@ -4,35 +4,38 @@ namespace App\Notifications;
 
 use Illuminate\Auth\Notifications\VerifyEmail as VerifyEmailNotification;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Messages\MailMessage;
-use Illuminate\Support\Facades\URL;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\URL;
 
 class VerifyEmail extends VerifyEmailNotification implements ShouldQueue
 {
     use Queueable;
+
+
     /**
      * Get the mail representation of the notification.
      *
      * @param mixed $notifiable
      * @return \Illuminate\Notifications\Messages\MailMessage
      */
-
-    public function toMail($notifiable): MailMessage
+    public function toMail($notifiable)
     {
-        $verificationUrl = $this->verificationUrl($notifiable);
-
-
-        $frontendUrl = config('app.frontend_url');
-
-
-        $redirectUrl = $frontendUrl . '/email/verify?token=' . urlencode($verificationUrl);
 
         return (new MailMessage)
-        ->subject('Verify Email Address')
-        ->greeting('Hello, ' . ucfirst($notifiable->first_name) . ' ' . ucfirst($notifiable->last_name) . '!')
-        ->line('Thank you for signing up with our App. To Ensure the security of your account, please verify your email address.')
-        ->action('Verify Email', $this->verificationUrl($notifiable))
-        ->line('If your did not create an account, no further action is required.');
+            ->subject('Verify Email Address')
+            ->greeting('Hello, ' . ucfirst($notifiable->first_name) . ' ' . ucfirst($notifiable->last_name) . '!')
+            ->line('Thank you for signing up with our App. To Ensure the security of your account, please verify your email address.')
+            ->action('Verify Email', $this->verificationUrl($notifiable))
+            ->line('If your did not create an account, no further action is required.');
     }
+
+    /**
+     * Get the verification URL for the given notifiable.
+     *
+     * @param mixed $notifiable
+     * @return string
+     */
+
 }
