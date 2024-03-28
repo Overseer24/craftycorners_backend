5004e173b8b078a59900ed2b5731049efc64386d
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-03-28 20:09:06 +0800

    modify

--- app/Http/Controllers/api/VerificationController.php
+++ app/Http/Controllers/api/VerificationController.php
@@ -24,22 +24,22 @@ class VerificationController extends Controller
         ]);
     }
 
-    public function resendVerificationEmail(Request $request)
-    {
-        if($request->user()->hasVerifiedEmail()){
-            return response()->json([
-                'message' => 'Email already verified'
-            ], 400);
-        }
+//    public function resendVerificationEmail(Request $request)
+//    {
+//        if($request->user()->hasVerifiedEmail()){
+//            return response()->json([
+//                'message' => 'Email already verified'
+//            ], 400);
+//        }
+//
+//        $request->user()->sendEmailVerificationNotification();
+//        return response()->json([
+//            'message' => 'Email verification link sent on your email id'
+//        ]);
+//    }
 
-        $request->user()->sendEmailVerificationNotification();
-        return response()->json([
-            'message' => 'Email verification link sent on your email id'
-        ]);
-    }
 
-
-    public function verifyEmail(Request $request, $id, $hash)
+    public function verifyEmail($id, $hash)
     {
         $user = User::find($id);
 
--- config/app.php
+++ config/app.php
@@ -59,7 +59,7 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
-    'frontend_url'=>env('FRONT_END_URL', 'http://localhost:3000'),
+    'frontend_url'=>env('FRONTEND_URL', 'http://localhost:3000'),
 
     'frontend' => [
         'url' => 'http://localhost:3000', // Adjust the URL accordingly
