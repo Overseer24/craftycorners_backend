c852375a0858fe9a5352f78dbd01d5c501f11193
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-04-01 20:01:13 +0800

    add community

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -11,7 +11,7 @@ class NotificationController extends Controller
     public function index(){
         $user = auth()->user();
         $notifications = $user->notifications()
-            ->with(['notifiable', 'relatedUser'])
+            ->with(['notifiable', 'relatedUser','notifiable.community'])
 //            ->whereNull('read_at')
             ->orderBy('created_at', 'desc')->paginate(5);
 
@@ -27,6 +27,11 @@ class NotificationController extends Controller
                     'id' => $notification->notifiable->id,
                     'title' => $notification->notifiable->title,
 
+
+                ],
+                'community'=>[
+                    'id'=>$notification->notifiable->community->id,
+                    'name'=>$notification->notifiable->community->name,
                 ],
                 'like' => [],
             ];
