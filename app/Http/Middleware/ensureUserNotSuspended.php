8d94f544ceec71135ce8d57f7e78af529070c006
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-04-09 02:22:57 +0800

    Update ensureUserNotSuspended.php

--- app/Http/Middleware/ensureUserNotSuspended.php
+++ app/Http/Middleware/ensureUserNotSuspended.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class ensureUserNotSuspended
@@ -17,11 +18,11 @@ class ensureUserNotSuspended
     {
 
         //fetch suspension date base on report
-        $user=auth()->user();
+        $user=Auth::user();
         if($user && $user->type === 'suspended'){
             $unsuspendDate = $user->reportedPosts()->where('resolution_option', 'suspend')->first()->unsuspend_date;
             //logout user
-            auth()->logout();
+
             return response()->json([
                 'message' => 'You are suspended until '.$unsuspendDate
             ], 403);
