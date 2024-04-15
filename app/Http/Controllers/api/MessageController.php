607f44442b3ae7089dda8a6a946192fd222d0cc2
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-04-15 11:04:02 +0800

    Update MessageController.php

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -74,7 +74,7 @@ class MessageController extends Controller
 
         if($request->hasFile('attachment')){
             $file =$request->file('attachment');
-            $filepath = $file->store('public/messages/conversation_'.$conversation->id.'/attachments');
+            $file->storeAs('public/messages/conversation_'.$conversation->id.'/attachments', $file->getClientOriginalName());
             $attachment = $message->attachments()->create([
                 'file_path' => 'messages/conversation_'.$conversation->id.'/attachments/'.$file->getClientOriginalName(),
                 'file_type' => $file->getClientMimeType()
