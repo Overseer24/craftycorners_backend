898660b2704993389934b8433b7c0addc9e397d7
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-04-21 19:44:43 +0800

    Update NotificationController.php

--- app/Http/Controllers/api/NotificationController.php
+++ app/Http/Controllers/api/NotificationController.php
@@ -10,7 +10,7 @@ class NotificationController extends Controller
 {
     public function index()
     {
-        $notifications = auth()->user()->notifications()->orderBy('created_at', 'desc')->get();
+        $notifications = auth()->user()->notifications()->orderBy('created_at', 'desc')->paginate(10);
 
         return response()->json($notifications);
     }
