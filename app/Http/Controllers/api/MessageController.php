b25fb64d211892a325244d6d449155f9022c9dee
Nixon <66957205+Overseer24@users.noreply.github.com>
2024-04-21 19:31:39 +0800

    Update MessageController.php

--- app/Http/Controllers/api/MessageController.php
+++ app/Http/Controllers/api/MessageController.php
@@ -124,7 +124,13 @@ class MessageController extends Controller
     public function getConversations(){
         $user = auth()->user();
 
-        $conversations= $user->conversations()->with('messages', 'receiver', 'sender')->get();
+        $conversations= $user->conversations()->with('messages', 'receiver', 'sender')->  ->addSelect(['latest_message_at' => Message::select('created_at')
+            ->whereColumn('conversation_id', 'conversations.id')
+            ->latest()
+            ->take(1)
+        ])
+            ->orderBy('latest_message_at', 'desc')
+            ->get();;
 
         return ConversationsListResource::collection($conversations);
 
