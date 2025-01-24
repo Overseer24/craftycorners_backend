536fbcb63ae184874cbe90d91b18fc62d4951d75
Nixon <nixon.somoza@gmail.com>
2025-01-25 02:17:30 +0800

    new laravel 11 with vuejs

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,7 @@
+<template >
+    <div class="flex items-center justify-center h-screen space-x-4">
+        <div class="size-60 rounded-full bg-conic/[in_hsl_longer_hue] from-red-600 to-red-600"></div>
+        <div class="size-60 rounded-full bg-radial-[at_25%_25%] from-white to-zinc-900 to-75%"></div>
+    </div>
+
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home');
 });
 
 Route::get('/email',function (){
